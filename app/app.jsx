4ce016497707476efdeef401782ce0f684480760
Daniel McGowan <mcgodan@nlg.nhs.uk>
2016-08-03 21:12:54 +0100

    add foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
